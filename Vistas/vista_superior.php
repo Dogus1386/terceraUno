f1d86136c1c153e98978ea79eb59909c18fe77af
Dogus1386 <139095795+Dogus1386@users.noreply.github.com>
2025-10-14 10:33:49 -0600

    Creacion de proyecto

---
+++ Vistas/vista_superior.php
@@ -0,0 +1,20 @@
+Vista Superior
+
+<!DOCTYPE html>
+<html lang="Spanish">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Tercera 1" content="Proyecto Universitario">
+    <title>Tercera 1</title>
+    <link rel="stylesheet" href="./css/main.css">
+
+
+
+
+
+</head>
+
+
+
+
---
+++ clinetes.php
@@ -0,0 +1,5 @@
+
+<?php require_once "./Vistas/vista_superior.php"?>
+Clientes
+
+<?php require_once "./Vistas/vista_inferior.php"?>
